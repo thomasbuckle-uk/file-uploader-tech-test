cbf6201563df53dc331f78589a21a943b744e430
Thomas Buckle <thomasbuckle.uk@outlook.com>
2021-09-06 15:55:40 +0100

    Install profiler pack and setup of files

--- src/Controller/FileUploadController.php
+++ src/Controller/FileUploadController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 class FileUploadController extends \Symfony\Bundle\FrameworkBundle\Controller\AbstractController
 {
+
     public function listFiles(
         Request $request
     ) :Response {
