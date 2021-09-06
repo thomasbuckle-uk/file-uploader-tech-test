dde04352ba44b11cee5b6240946afb559d424dbb
Thomas Buckle <thomasbuckle.uk@outlook.com>
2021-09-06 14:46:33 +0100

    Initial Commit Project setup & composer require api

---
+++ src/Controller/FileUploadController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controller;
+
+class FileUploadController extends \Symfony\Bundle\FrameworkBundle\Controller\AbstractController
+{
+    public function listFiles(
+        Request $request
+    ) :Response {
+
+    }
+
+}
